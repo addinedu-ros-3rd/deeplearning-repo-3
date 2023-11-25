107a9a0ae3fd161d7d7183333e9de7e93a57910b
HongKi Cho <whghdrl9977@gmail.com>
2023-11-25 18:13:35 +0900

    test: cam test

--- roi.py
+++ roi.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import cv2
 import numpy as np
 from datetime import datetime
@@ -10,12 +10,20 @@ from Camera import Camera
 
 # 미리 video 폴더 생성 안해두면 안됨
 file_folder = os.path.dirname(os.path.abspath(__file__))
-from_class = uic.loadUiType(os.path.join(os.path.dirname(file_folder) +\
+from_class = uic.loadUiType(os.path.join(file_folder +\
                             "/camera.ui"))[0]
+
+
+class Polygon:
+    def __init__(self, points):
+        self.points = points
+
+
 
 class WindowClass(QMainWindow, from_class) :
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
     
         self.image = None
         self.img_modified = None
@@ -24,14 +32,47 @@ class WindowClass(QMainWindow, from_class) :
         self.camera = Camera(self)
         self.camera.daemon = True
         self.isCameraOn = False
-        self.video = None
 
-        self.text = ""
-        self.lineEdit.setText(self.text)
+        self.polyList = []
 
-        self.btnCam.clicked.connect(self.clickCamera)
+
         self.camera.update.connect(self.updateCamera)
         self.btnCapture.clicked.connect(self.clickCapture)
+
+        self.label.mousePressEvent = self.getPixel
+        self.pol = []
+
+        self.cap = cv2.VideoCapture(1)
+        print("Loading cam", end='')
+        while not self.cap.isOpened():
+            print(".")
+
+
+    def addPolygon(self):
+        w, h = self.label.width(), self.label.height()
+        painter = QPainter(self.img_modified)
+
+        painter.setPen(QPen(Qt.black, 5, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.red, Qt.VerPattern))
+
+        points = [QPoint(w-50, h-50), QPoint(w+50, h-50),
+                  QPoint(w+50, h+50), QPoint(w-50, h+50)]
+        
+        painter.drawPolygon(QPolygon(points))
+        
+        self.plotImg()
+        self.polyList.append(Polygon)
+
+
+    def getPixel(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+
+        # self.pol.append(QPoint(int(x),int(y)))
+
+        # print(x,y, self.pol)
+
+        self.statusBar().showMessage("x: " + str(x) + ", y: " + str(y))
 
 
     # Camera Events
@@ -48,16 +89,10 @@ class WindowClass(QMainWindow, from_class) :
             self.btnCam.setText('Camera on')
             self.btnCam.setStyleSheet("")
 
-            self.isCannyPressed = False
-            self.btnCanny.setStyleSheet("")
-
             self.isCameraOn = False
             self.btnCapture.setText("Save")
 
             self.cameraStop()
-
-            self.btnRec.setText('Rec on')
-            self.recordingStop()
 
     def cameraStart(self):
         self.camera.running = True
@@ -80,19 +115,14 @@ class WindowClass(QMainWindow, from_class) :
             self.plotImg()
 
     def clickCapture(self):
-        if type(self.image) != np.ndarray:
-            self.resetCanny()
-            self.resetHSV()
-            self.resetRGB()
+        retval, self.image = self.cap.read()
+        if retval:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+            self.img_modified = self.image
+            self.plotImg()
 
-            retval, self.image = cv2.VideoCapture(-1).read() 
-            if retval:
-                self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-                self.img_modified = self.image
-                self.plotImg()
-
-            else:
-                return
+        else:
+            return
 
 
 
@@ -107,3 +137,11 @@ class WindowClass(QMainWindow, from_class) :
                                         Qt.KeepAspectRatio)
 
         self.label.setPixmap(self.pixmap)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindows = WindowClass()
+    myWindows.show()
+
+    sys.exit(app.exec_())
