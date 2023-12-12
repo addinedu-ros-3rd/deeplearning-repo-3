11218ec7bd025622a33c4f587234e0c349db52cc
wildcatts <wildcatts94@gmail.com>
2023-12-12 15:13:25 +0900

    add : db,log fix : status, update_db

--- main/update_db.py
+++ main/update_db.py
@@ -1,38 +1,109 @@
+from datetime import datetime
+import DB
+import Logger
+import read_rfid
+
+now = datetime.now()
+# now_ts = now.strftime('%Y-%m-%d %H:%M:%S')  # 서버의 시간을 보관 = 서버 시간이 UTC이면 UTC로 보관(로컬 테스트 시 한국 시간 저장됨)
+
+db = DB()
+log = Logger('DB_log.log')
+# ser = serial.Serial("/dev/ttyACM0", 9600)  # rfid와 시리얼 통신 필요
+
 # 고객 table에서 고객 정보 가져오기
 def get_customer_info():
-    pass
+    try:
+        query = "select customerID, personality from customer"
+        db.execute(query)
+
+        customer_ID = db.fetchone(query)
+        personality = db.fetchOne(query)
+
+        return customer_ID, personality
+    
+    except Exception as e:
+        log.error(f"get_customer_info() is error")
+        return None 
 
 
 # 출입 table : check-in 상태 logging
 def log_checkIn_state(checkIn_time, customer_info):
-    pass
+    try:
+        query = "insert into enterence (customer_ID, enterenceTime, enterStatus) values (%s, %s, %d)"
+        db.execute(query, (customer_info, checkIn_time, 1))
+    except Exception as e:
+        log.error(f"log_checkIn_state() is error")
+    
 
 
 # 출입 table : check-out 상태 logging
 def log_checkOut_state(checkOut_time, customer_info):
-    pass
+    try:
+        query = "insert into enterence (customer_ID, enterenceTime, totalAmount) values (%s, %s, %d)"
+        db.execute(query, (customer_info, checkOut_time, 0))
+    except Exception as e:
+        log.error(f"log_checkOut_state() is error")
+    
 
 
-# 결제 내역 table : update
+# 결제 내역 table : logging
 def make_payment(customer):
-    pass
+    customer_ID = customer.id
+    checkOut_time = customer.out_time
+    price = customer.total_price
+    try:
+        query = "insert into payment (customer_ID, enterenceTime, totalAmount) values (%s, %s, %d)"
+        db.execute(query, (customer_ID, checkOut_time, price))
+    except Exception as e:
+        log.error(f"make_payment() is error")
+    
 
 
 # 행동 인식 table : logging
 def log_action_state(action_time, action_type, fruit_id):
-    pass
+    try:
+        query = "insert into actionRecognition (fruitID, actionTime, actionType, totalAmount) values (%d, %s, %s)"
+        db.execute(query, (fruit_id, action_time, action_type))
+    except Exception as e:
+        log.error(f"log_action_state() is error")
+    
 
 
-# 불일치 table : logging
-def log_mismatch(mismatch_time):
-    pass
+# # 불일치 table : logging
+# def log_mismatch(customer, mismatch_time):
+#     try:
+#         query = "insert into mismatchActionStand (standFruitID, actionFruitID, mismatch) values (%d, %d, %s)"
+#         db.execute(query, (fruit_id, action_time, action_type))
+#     except Exception as e:
+#         log.error(f"make_payment() is error")
+    
 
 
 # 과일 table : 재고 update
-def update_fruit_stock():
-    pass
+def update_fruit_stock(fruit_type, fruit_values):
+    try:
+        query = "select stockStand from fruits where fruitID=%d"
+        db.execute(query, fruit_type)
+
+        fruit_counts = db.fetchone(query)
+
+        query = "update fruits set stockStand=%d where fruitID=%s"
+        db.execute(query, ((fruit_counts-fruit_values), fruit_type))
+
+    except Exception as e:
+        log.error(f"update_fruit_stock() is error")
+    
 
 
 # 과일 table : 과일 가격 조회
-def get_price_list():
-    pass
+def get_price_list(fruit_type):
+    try:
+        query = "select price from fruits where fruitID=%d"
+        db.execute(query, fruit_type)
+
+        price = db.fetchone(query)
+        return price
+    
+    except Exception as e:
+        log.error(f"get_price_list() is error")
+        return None
